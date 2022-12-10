d3e6bb06dcaf6014379061f55b30498e688045bd
clocwerk <65974401+clocwerk@users.noreply.github.com>
2022-12-10 07:23:53 +0200

    Updates

---
+++ precache-manifest.8660428a153d847d95a6a1468a99ccf7.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "1e32803c0a0cd0914082",
+    "url": "/gpt3/static/css/main.dbdd4d64.chunk.css"
+  },
+  {
+    "revision": "1e32803c0a0cd0914082",
+    "url": "/gpt3/static/js/main.1e32803c.chunk.js"
+  },
+  {
+    "revision": "6c75759b56f0b906c1b6",
+    "url": "/gpt3/static/js/1.6c75759b.chunk.js"
+  },
+  {
+    "revision": "6e76edef2f72c93e8b6f",
+    "url": "/gpt3/static/js/runtime~main.6e76edef.js"
+  },
+  {
+    "revision": "a95f42a40ce776c07a2891c220f620ae",
+    "url": "/gpt3/static/media/logo.a95f42a4.svg"
+  },
+  {
+    "revision": "3d84899326ac5f0f66149fa037693d18",
+    "url": "/gpt3/static/media/people.3d848993.png"
+  },
+  {
+    "revision": "17ade55739aedbbb39e78442cdad6fb3",
+    "url": "/gpt3/static/media/ai.17ade557.png"
+  },
+  {
+    "revision": "3c84ae2642b58c3bc28340a7e763fc23",
+    "url": "/gpt3/static/media/possibility.3c84ae26.png"
+  },
+  {
+    "revision": "bc656ca823d98e469cbe7b8ab2edda5b",
+    "url": "/gpt3/index.html"
+  }
+];
